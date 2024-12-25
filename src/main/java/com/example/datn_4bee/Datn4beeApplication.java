fcfc2d3c6fe6726f6b6146aeebb55ad5a221687d
thinhnongdev <tienthinhkk@gmail.com>
2024-12-25 19:34:10 +0700

    first commit

---
+++ src/main/java/com/example/datn_4bee/Datn4beeApplication.java
@@ -0,0 +1,13 @@
+package com.example.datn_4bee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datn4beeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Datn4beeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datn_4bee/Datn4beeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datn_4bee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datn4beeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
