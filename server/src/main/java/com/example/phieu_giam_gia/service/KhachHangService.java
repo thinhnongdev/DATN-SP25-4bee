b0612b48fb1014160ee62d1ffcaa52a9cc3f2e2c
nguyen van nam <nguyennamdev99@gmail.com>
2025-02-12 23:24:03 +0700

    Xóa thư mục service

--- server/src/main/java/com/example/phieu_giam_gia/service/KhachHangService.java
+++
@@ -1,9 +0,0 @@
-package com.example.phieu_giam_gia.service;
-import com.example.phieu_giam_gia.dto.KhachHangDTO;
-import com.example.phieu_giam_gia.entity.KhachHang;
-
-import java.util.List;
-
-public interface KhachHangService {
-    List<KhachHangDTO> getAll();
-}
