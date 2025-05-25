11e6f814fa73bdbaeab8ddc9ef175bcb3eabfdd8
Thinh Nong <138519443+thinhnongdev@users.noreply.github.com>
2025-05-25 09:57:53 +0700

    Update VoucherClientController.java

--- server/src/main/java/com/example/server/controller/Client/VoucherClientController.java
+++ server/src/main/java/com/example/server/controller/Client/VoucherClientController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://datn-sp-25-4bee.vercel.app", allowCredentials = "true")
 public class VoucherClientController {
     @Autowired
     PhieuGiamGiaService phieuGiamGiaService;
