a9b59617377ffe2a39bd24adcff8cd3ce21c9811
Thinh Nong <138519443+thinhnongdev@users.noreply.github.com>
2025-05-25 09:59:32 +0700

    Update LichSuHoaDonController.java

--- server/src/main/java/com/example/server/controller/HoaDon/LichSuHoaDonController.java
+++ server/src/main/java/com/example/server/controller/HoaDon/LichSuHoaDonController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/lich-su-hoa-don")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://datn-sp-25-4bee.vercel.app", allowCredentials = "true")
 public class LichSuHoaDonController {
     private final LichSuHoaDonService service;
 
