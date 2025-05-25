621f0c5ed79371ba53cd70c844818830353d85d6
Thinh Nong <138519443+thinhnongdev@users.noreply.github.com>
2025-05-25 09:55:45 +0700

    Update DiaChiClientController.java

--- server/src/main/java/com/example/server/controller/Client/DiaChiClientController.java
+++ server/src/main/java/com/example/server/controller/Client/DiaChiClientController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://datn-sp-25-4bee.vercel.app", allowCredentials = "true")
 public class DiaChiClientController {
     @Autowired
     KhachHangService khachHangService;
